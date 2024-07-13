9ac19034117ccb0ef0e601d31cdcac6552729699
Roberto Junior <robertonsj31@gmail.com>
2024-07-12 23:06:41 -0300

    Calculator bug fixing is done!

--- includes/calculater.php
+++ includes/calculater.php
@@ -4,14 +4,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Calculator</title>
+
+  <style>
+
+  </style>
 </head>
 
 <body>
+
+  <h3>Calculator</h3>
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <input type="number" name="num01" placeholder="First Operand">
 
     <select name="operator">
+      <option value="" selected disabled>Choose</option>
       <option value="add">+</option>
       <option value="subtract">-</option>
       <option value="multiply">*</option>
@@ -23,7 +30,35 @@
     <button>Calculate</button>
 
     <?php
-      if
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      //Grab data from inputs
+      $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
+      $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+      $operator = htmlspecialchars($_POST["operator"]);
+
+      // Errors handlers
+      if(is_numeric($num01) && is_numeric($num02) && !empty($operator)){
+        
+        if ($num02 == 0 && $operator == "divide") {
+          echo "<p style='color: red';>Error. Division by zero.</p>";
+  
+        } else {
+          // Calculate the numbers if no erros
+          $result = match ($operator) {
+            "add" => $num01 + $num02,
+            "subtract" => $num01 - $num02,
+            "multiply" => $num01 * $num02,
+            "divide" => $num01 / $num02,
+          };
+          echo "<br><br>RESULT: ", $result;
+        }
+
+      }else {
+        echo "<p style='color: red';>Set all fields properly.</p>";
+      }
+
+    }
     ?>
 
   </form>
